d49b987e5dcf7d7e6a27be6779bbd214dd62ce1b
Ryan Zurrin <rzurrin@gmail.com>
2021-12-14 19:54:53 -0500

    updated index and gofundme image

--- php/contact.php
+++ php/contact.php
@@ -39,7 +39,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@example.net";
-$address = "example@example.net";
+$address = "ryanzurrin@ryanzurrin.com";
 
 
 // Configuration option.
@@ -82,3 +82,4 @@ if(mail($address, $e_subject, $msg, $headers)) {
 	echo 'ERROR!';
 
 }
+
