3a1690b8a415e30bde9307fade0d0e5fe42a3713
Ryan Zurrin <ryanzurrin@ryanzurrin.com>
2022-07-06 22:42:07 -0400

    added some js to change color of my name on hover, also added new section to journal area.

---
+++ js/myScripts.js
@@ -0,0 +1,26 @@
+"use strict";
+// when someone hovers over id=myName, change to random colors every 2 seconds
+myName = document.getElementById("myName");
+console.log(myName);
+
+myName.addEventListener("mouseover", function () {
+  // create a list of light colors to randomly choose from
+  let colors = [
+    "#00adef",
+    "#e15ff5",
+    "#f9a825",
+    "#6eff7d",
+    "#2de3b0",
+    "#d43d4c",
+    "#f0f55b",
+    "#f5b914",
+    "#f252a2",
+    "#68f545",
+  ];
+  myName.style.color = colors[Math.floor(Math.random() * colors.length)];
+});
+
+// reset the color back to white when not hovering over
+myName.addEventListener("mouseout", function () {
+  myName.style.color = "white";
+});
