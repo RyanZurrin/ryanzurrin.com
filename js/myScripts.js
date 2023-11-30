33fe91e6a22ab935fb3b9316d952d10260dd2a22
Ryan Zurrin <ryanzurrin@ryanzurrin.com>
2023-11-30 17:54:13 -0500

    updated website to change Name to different text quotes and added some new pics.

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,47 +1,70 @@
-let images = [
-  "images/me2.jpg",
-  "images/me3.jpg",
-  "images/me4.jpg",
-  "images/me5.jpg",
-];
-
 window.onload = function () {
-  myName = document.getElementById("myName");
-  myPic = document.getElementById("myPic");
+  let myName = document.getElementById("myName");
+  let helloworld = document.getElementById("helloworld");
+  let myPic = document.getElementById("myPic");
+
+  let nameTexts = [
+    { main: "In Code We Trust", hello: "" },
+    { main: "Dreaming Digital", hello: "I stay 1010 0101" },
+    { main: "Software Visionary", hello: "A true" },
+    { main: "Software Engineer", hello: "I am a" },
+    { main: "Computer Science Major", hello: "I am a" },
+    { main: "Web Developer", hello: "I am a" },
+    { main: "Dad", hello: "A proud" },
+    { main: "Success, against all odds", hello: "I have achieved" },
+    { main: "Bioinformatics", hello: "I Work in" },
+  ];
+
+  let colors = [
+    "#00adef",
+    "#e15ff5",
+    "#f9a825",
+    "#6eff7d",
+    "#2de3b0",
+    "#d43d4c",
+    "#f0f55b",
+    "#f5b914",
+    "#f252a2",
+    "#68f545",
+  ];
+
+  let images = [
+    "images/me1.jpg",
+    "images/me2.jpg",
+    "images/me3.jpg",
+    "images/me4.jpg",
+    "images/me5.jpg",
+    "images/ryderandme.jpg",
+  ];
 
   myName.addEventListener("mouseover", function () {
-    // create a list of light colors to randomly choose from
-    let colors = [
-      "#00adef",
-      "#e15ff5",
-      "#f9a825",
-      "#6eff7d",
-      "#2de3b0",
-      "#d43d4c",
-      "#f0f55b",
-      "#f5b914",
-      "#f252a2",
-      "#68f545",
-    ];
+    let randomIndex = Math.floor(Math.random() * nameTexts.length);
     myName.style.color = colors[Math.floor(Math.random() * colors.length)];
+
+    // Randomly change text with a certain probability, e.g., 50%
+    if (Math.random() < 0.75) {
+      helloworld.textContent = nameTexts[randomIndex].hello;
+      myName.textContent = nameTexts[randomIndex].main;
+    }
+
     myName.style.fontSize = "1.5em";
     myName.style.fontFamily = "cursive";
   });
 
-  // reset the color back to white when not hovering over
   myName.addEventListener("mouseout", function () {
+    helloworld.textContent = "Hello World! My name is,";
+
+    myName.textContent = "Ryan Zurrin";
     myName.style.color = "white";
     myName.style.fontSize = "33px";
     myName.style.fontFamily = "Montserrat, sans-serif";
   });
 
   myPic.addEventListener("mouseover", function () {
-    // change the picture to a different one
-    path = Math.floor(Math.random() * images.length);
-    myPic.src = images[path];
+    myPic.src = images[Math.floor(Math.random() * images.length)];
   });
 
   myPic.addEventListener("mouseout", function () {
-    myPic.src = "images/ryanzaboutme.jpg";
+    myPic.src = "images/ryanaboutme.jpg";
   });
 };
